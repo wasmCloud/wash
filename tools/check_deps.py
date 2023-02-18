98878447dd6bb5c1b96e6cd1f7d38538ea71dedd
Matt Wilkinson <matt@mattwilkinson.dev>
2023-02-18 11:16:25 -0500

    Fix deps script some more
    Signed-off-by: Matt Wilkinson <matt@mattwilkinson.dev>

--- tools/check_deps.py
+++ tools/check_deps.py
@@ -1,4 +1,3 @@
-import os
 import shutil
 import subprocess
 
@@ -17,14 +16,14 @@ if tinygo is None:
     print('tinygo not found. Please install it from https://tinygo.org/')
     exit(1)
 
-targets = subprocess.run("rustup target list", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout
+targets = subprocess.run("rustup target list --installed", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout
 if "wasm32-unknown-unknown" not in targets:
     print('Rust wasm32-unknown-unknown target not found. Installing..."')
-    os.system('rustup target add wasm32-unknown-unknown')
+    subprocess.run('rustup target add wasm32-unknown-unknown', shell=True)
 
 nextest_output = subprocess.run("cargo nextest --version", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout
 if "error: no such command" in nextest_output:
     print('cargo nextest not found. Installing..."')
-    os.system('cargo install cargo-nextest --locked')
+    subprocess.run('cargo install cargo-nextest --locked', shell=True)
 
 print("All dependencies are installed!")
