4b9380593ef2376eebd9ec8b7587d6bf9b56854e
Matt Wilkinson <matt@mattwilkinson.dev>
2023-02-18 11:13:28 -0500

    use subprocess in check_deps
    Signed-off-by: Matt Wilkinson <matt@mattwilkinson.dev>

--- Makefile
+++ Makefile
@@ -33,7 +33,7 @@ clean:
 
 # Checks dependencies necessary for developing wash.
 check-deps:
-	python3 ./tools/check-deps.py
+	python3 tools/check_deps.py
 
 help:  ## Display this help
 	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_\-.*]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
--- tools/check_deps.py
+++ tools/check_deps.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 cargo = shutil.which('cargo')
 if cargo is None:
@@ -16,9 +17,14 @@ if tinygo is None:
     print('tinygo not found. Please install it from https://tinygo.org/')
     exit(1)
 
-targets = os.popen('rustup target list').read()
+targets = subprocess.run("rustup target list", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout
 if "wasm32-unknown-unknown" not in targets:
     print('Rust wasm32-unknown-unknown target not found. Installing..."')
     os.system('rustup target add wasm32-unknown-unknown')
 
-os.system('cargo install cargo-nextest --locked')
+nextest_output = subprocess.run("cargo nextest --version", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True).stdout
+if "error: no such command" in nextest_output:
+    print('cargo nextest not found. Installing..."')
+    os.system('cargo install cargo-nextest --locked')
+
+print("All dependencies are installed!")
