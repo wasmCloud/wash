c08064fa5cc0e9b9d257002c6d6e3eff0e71ecd6
Brooks Townsend <brooks@cosmonic.com>
2023-05-05 11:47:01 -0400

    removed extra commented test
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- crates/wash-lib/src/start/wadm.rs
+++ crates/wash-lib/src/start/wadm.rs
@@ -235,36 +235,4 @@ mod test {
         let _ = remove_dir_all(install_dir).await;
         Ok(())
     }
-
-    //     #[tokio::test]
-    //     async fn can_gracefully_fail_running_nats() -> Result<()> {
-    //         let install_dir = temp_dir().join("can_gracefully_fail_running_nats");
-    //         let _ = remove_dir_all(&install_dir).await;
-    //         create_dir_all(&install_dir).await?;
-    //         assert!(!is_nats_installed(&install_dir).await);
-
-    //         let res = ensure_nats_server(NATS_SERVER_VERSION, &install_dir).await;
-    //         assert!(res.is_ok());
-
-    //         let config = NatsConfig::new_standalone("127.0.0.1", 10003, Some("extender".to_string()));
-    //         let nats_one = start_nats_server(
-    //             &install_dir.join(NATS_SERVER_BINARY),
-    //             std::process::Stdio::null(),
-    //             config.clone(),
-    //         )
-    //         .await;
-    //         assert!(nats_one.is_ok());
-
-    //         // Give NATS a few seconds to start up and listen
-    //         tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
-    //         let log_path = install_dir.join("nats.log");
-    //         let log = std::fs::File::create(&log_path)?;
-    //         let nats_two = start_nats_server(&install_dir.join(NATS_SERVER_BINARY), log, config).await;
-    //         assert!(nats_two.is_err());
-
-    //         nats_one.unwrap().kill().await?;
-    //         let _ = remove_dir_all(install_dir).await;
-
-    //         Ok(())
-    //     }
 }
