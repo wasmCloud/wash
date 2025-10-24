5817ffa4ef896e88f19bbfc0a41aef8784fb6e65
Brooks Townsend <brooks@cosmonic.com>
2025-10-24 10:51:37 -0400

    feat(cron-service): make main async to show it can be done
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- examples/cron-service/src/main.rs
+++ examples/cron-service/src/main.rs
@@ -2,10 +2,13 @@ wit_bindgen::generate!({
     world: "service"
 });
 
-fn main() {
+// NOTE: This example is a `tokio::main` to show how you can use an async main, but
+// it can just be a synchronous main as well.
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     eprintln!("Starting cron-service with 1 second intervals...");
     loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
         let _ = wasmcloud::example::cron::invoke();
     }
 }
