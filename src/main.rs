350b77cda6590fe6de46da83012767cc7753a865
Bailey Hayes <behayes2@gmail.com>
2025-11-08 13:14:51 -0500

    fix: noninteractive check
    Fixes #142
    
    Signed-off-by: Bailey Hayes <behayes2@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -235,7 +235,7 @@ async fn main() {
     let mut stdout_buf = BufWriter::new(stdout);
 
     // Recommend a new version of wash if available
-    if !cli.non_interactive &&
+    if !non_interactive &&
         ctx
         .check_new_version()
         .await
