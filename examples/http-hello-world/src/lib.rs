d7881e3ebbed71daec3bd40344179e156320772f
Eric Gregory <eric@cosmonic.com>
2025-12-17 10:31:22 -0500

    chore(examples): refactor http-hello-world to use wstd (#206)
    Signed-off-by: Eric Gregory <eric@cosmonic.com>

--- examples/http-hello-world/src/lib.rs
+++ examples/http-hello-world/src/lib.rs
@@ -1,13 +1,21 @@
-use wasmcloud_component::http;
+use wstd::http::{Body, Request, Response, StatusCode};
 
-struct Component;
-
-impl http::Server for Component {
-    fn handle(
-        _request: http::IncomingRequest,
-    ) -> http::Result<http::Response<impl http::OutgoingBody>> {
-        Ok(http::Response::new("Hello from wasmCloud!\n"))
+#[wstd::http_server]
+async fn main(req: Request<Body>) -> Result<Response<Body>, wstd::http::Error> {
+    match req.uri().path_and_query().unwrap().as_str() {
+        "/" => home(req).await,
+        _ => not_found(req).await,
     }
 }
 
-http::export!(Component);
+async fn home(_req: Request<Body>) -> Result<Response<Body>, wstd::http::Error> {
+    // Return a simple response with a string body
+    Ok(Response::new("Hello from wasmCloud!\n".into()))
+}
+
+async fn not_found(_req: Request<Body>) -> Result<Response<Body>, wstd::http::Error> {
+    Ok(Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body("Not found\n".into())
+        .unwrap())
+}
